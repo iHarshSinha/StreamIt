f006175592bd8ea835e9702b6f8731f9f43f1c0d
iHarshSinha <harsh.sinha@iiitb.ac.in>
2026-01-16 18:29:46 +0530

    Initial Project with security and jpa

---
+++ src/main/java/com/streamit/groupchatapp/GroupChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.streamit.groupchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupChatAppApplication.class, args);
+    }
+
+}
